658caef1466a137ff8bbebb24a93451cbaa6d90b
Brian Palacios <b.estpalacios@gmail.com>
2023-01-31 17:43:03 -0600

    save high score change

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", mode='w') as data:
+            self.high_score = int(data.read())
+
         self.color("white")
         self.penup()
         self.goto(x=0, y=265)
@@ -15,13 +18,23 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def add_point(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(x=0, y=0)
-        self.write(arg=f"Game Over", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        # update highscore
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        # reset the score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(x=0, y=0)
+    #     self.write(arg=f"Game Over", align=ALIGNMENT, font=FONT)
