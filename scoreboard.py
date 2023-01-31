9389c631fcc1fc9e1232aed90f851625bfe454c8
Brian Palacios <b.estpalacios@gmail.com>
2023-01-31 17:46:29 -0600

    fix read bug

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open("data.txt", mode='w') as data:
+        with open("data.txt", mode='r') as data:
             self.high_score = int(data.read())
 
         self.color("white")
